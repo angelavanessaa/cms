41df7283f253cbd78abacbc0021e1b06e6f28558
Angela Vanessa <angelavanessa@Angelas-iMac.local>
2019-05-13 23:51:05 +0700

    categories crud with jquery modal.

---
+++ resources/views/partials/_sidenav.blade.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-2">
+            <div class="list-group">
+                <li class="list-group-item">
+                    <a href="#">Posts</a>
+                </li>
+                <li class="list-group-item">
+                    <a href="/categories">Categories</a>
+                </li>
+            </div>
+        </div>
+
+        <div class="col-md-10">
+            @yield('content')
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello world</h1>
+    asdasdasd
 @endsection
